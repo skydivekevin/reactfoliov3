fd354ef344a5c73d6b84c0e894cd99f6831c1b6c
skydivekevin <skydivekevin@gmail.com>
2019-06-29 09:08:23 -0600

    cleaned up landing page logic, mostly with scrollToRef func
    Signed-off-by: skydivekevin <skydivekevin@gmail.com>

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,50 +1,32 @@
 import React, { useEffect } from "react";
-import arrows from '../../Icons/arrows.svg'
-import { TweenMax } from 'gsap'
+import arrows from "../../Icons/arrows.svg";
+import { TweenMax } from "gsap";
 
 import "../../../src/App.css";
 
 const LandingPage = () => {
   useEffect(() => {
     TweenMax.from(".arrows", 1, { y: -30, repeat: "loop" });
-
   }, []);
 
-
-
-
-  const scrollToRef = () => window.scrollTo(0, window.innerHeight)
-  const useMountEffect = (fun) => useEffect(fun, [])
-
-
-  // const myRef = useRef(null)
-
-  useMountEffect(() => scrollToRef())
-
-
-
-
-
-
-  const arrowClick = () => {
-    scrollToRef()
-  }
+  const scrollToRef = () =>
+    window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
 
   return (
-    <div className='landing-page'>
-      <div className='intro-wrapper'>
-        <div className='intro-name'>Kevin Potts</div>
-        <div className='tagline'>
-          Full Stack Dev | Teacher | Make things work(er)
+    <React.Fragment>
+      <div className='landing-page'>
+        <div className='intro-wrapper'>
+          <div className='intro-name'>Kevin Potts</div>
+          <div className='tagline'>
+            Full Stack Dev | Teacher | Make things work(er)
+          </div>
+        </div>
+        <div className='arrows-container' onClick={scrollToRef}>
+          <img src={arrows} className='arrows' alt='arrowsSVG' />
         </div>
       </div>
-      {/* <button onClick={() => scrollToRef(myRef)} >Click to scroll </button> */}
-      <div className="arrows-container" onClick={arrowClick}>
-        <img src={arrows} className="arrows" alt="logo" />
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
-
 
 export default LandingPage;
